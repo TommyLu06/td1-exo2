0eb28fc8dec866350b350bfe90601a6b09519b45
TommyLu06 <67933350+TommyLu06@users.noreply.github.com>
2020-07-09 13:55:42 +1100

    Fichiers initiaux pour un projet MakeCode

---
+++ main.ts
@@ -0,0 +1,9 @@
+for (let index = 0; index < 2; index++) {
+    basic.showString("Bonjour")
+    basic.showNumber(10853)
+    basic.showArrow(ArrowNames.South)
+    basic.showIcon(IconNames.Happy)
+}
+basic.forever(function () {
+	
+})
